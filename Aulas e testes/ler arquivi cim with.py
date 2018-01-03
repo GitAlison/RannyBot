699553d78a366418eda313dcab7b1d836471ff6c
GitAlisonAguiar <alysson.k888@gmail.com>
2018-01-03 02:46:28 -0200

    deletando 3 pastas

--- Aulas e testes/ler arquivi cim with.py
+++
@@ -1,2 +0,0 @@
-with open("numeros.txt") as arq_numero:
-    print(arq_numero.read())
--- Aulas e testes/testes1.py
+++
@@ -1,5 +0,0 @@
-arquivo= open("Novo.txt","w")
-
-for linha in range(1,100):
-    arquivo.write("\n[{:2}]".format(linha))
-arquivo.close()
